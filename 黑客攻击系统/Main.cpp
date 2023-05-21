d0f570c553b7680be8a9bd6ca068f06a1a12dc4e
zsl9090 <1554960734@qq.com>
2023-05-21 17:08:00 +0800

    实现主函数。

---
+++ 黑客攻击系统/Main.cpp
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_NONSTDC_NO_DEPRECATE 1//strupr等不安全函数
+
+
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "hello world" << endl;
+	return 0;
+}
